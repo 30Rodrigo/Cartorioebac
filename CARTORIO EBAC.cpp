32be0823d507fdaf5698340b18d7223ca205e0a2
30Rodrigo <149726326+30Rodrigo@users.noreply.github.com>
2023-11-02 22:09:38 -0300

    Add files via upload

--- CARTORIO EBAC.cpp
+++ CARTORIO EBAC.cpp
@@ -133,6 +133,7 @@ int main()
 	 printf("\t1 - Registrar nomes\n");
 	 printf("\t2 - Consultar nomes\n");
 	 printf("\t3 - Deletar nomes\n");
+	 printf("\t4 - Sair\n\n");
 	 printf("Opção: "); // fim do menu
 	
 	 scanf("%d", &opcao); // armazenando a escolha do usuario 
@@ -153,6 +154,11 @@ int main()
 		
 		case 3:
 		deletar();
+		break;
+		
+		case 4:
+		printf("Obrigado por ultilizar o sistema!\n");
+		return 0;
 		break;
 		
 		default:
